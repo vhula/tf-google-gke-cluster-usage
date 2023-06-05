dfe462a710b6cfb014c2687238f303a664e3ca1e
Vadym Hula <vadym.hula@gmail.com>
2023-06-05 16:41:54 +0300

    Store terraform state in the bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "refreshing-oven-388713"
+    prefix = "terraform/state"
+  }
+}
+
 module "gke_cluster" {
   source         = "github.com/vhula/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
